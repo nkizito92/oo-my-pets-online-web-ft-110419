07360d287d5859f3f2c5a8b668c9175edf56943b
Kizito Njoku <nkizito57@gmail.com>
2019-11-20 04:03:03 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species 
+  attr_accessor :owner 
+  
+  
+    @@owner = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@owner << self
+  end 
+  
+  def say_species
+    "I am a #{@species}."
+  end 
+  
+  # def save 
+  # @@all << self 
+  # end 
+  def self.all
+    @@owner
+  end 
+  
+  def self.count 
+    @@owner.count
+  end 
+  
+  def self.reset_all
+    @@owner.clear
+  end 
 end
