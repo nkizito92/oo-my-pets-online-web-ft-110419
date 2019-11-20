7876b4f18dc2cb492ba8e12a111a010295d0428f
Kizito Njoku <nkizito57@gmail.com>
2019-11-20 15:57:58 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -7,6 +7,7 @@ class Owner
     @species = "human"
     @@owner << self
     @@all = []
+    @cats = []
   end 
   
   def say_species
@@ -26,6 +27,14 @@ class Owner
   end 
   
   def cats 
-    
+    Cat.all.select {|cat| cat.owner == self}
+  end 
+  
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end 
+  
+  def buy_cat(cat_bought)
+    cat_bought.owner = self
   end 
 end
